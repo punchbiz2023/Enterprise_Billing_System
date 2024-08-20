964497ae64c41633146069adee0378bf3020ef06
Bhuvanesh-23 <bhuvaneshm.ai@gmail.com>
2024-08-20 10:24:06 +0530

    After installing tailwind

--- tally/src/App.jsx
+++ tally/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Login from './components/login/login.jsx';
 import SignUp from './components/login/Signup.jsx';
 import Dashboard from './components/dashboard/dashboard.jsx';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
--- tally/src/components/dashboard/dashboard.jsx
+++ tally/src/components/dashboard/dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-// Make sure to create and style this CSS file
+
+
 
 const Dashboard = () => {
   return (
-    <div className="dashboard-container">
-      <h1>Punchbviz</h1>
-    </div>
+    <>
+      <div>
+        
+      </div>
+    </>
   );
 };
 
