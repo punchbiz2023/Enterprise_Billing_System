fc7dc68fedbe68e210ba3a767317c69334d0cdd6
Bhuvanesh-23 <bhuvaneshm.ai@gmail.com>
2024-08-24 17:10:34 +0530

    Added css to the login and signup page

--- tally/src/components/login/Signup.jsx
+++ tally/src/components/login/Signup.jsx
@@ -27,7 +27,7 @@ const SignUp = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Form validation
+
     if (!formData.companyName || !formData.companyEmail || !formData.password || !formData.confirmPassword || !formData.address || !formData.phoneNumber || !formData.documents) {
       toast.error("All fields are required!", {
         position: toast.POSITION.TOP_CENTER,
@@ -42,101 +42,113 @@ const SignUp = () => {
       return;
     }
 
-    // Show success notification
+
     toast.success("Company created successfully!", {
       position: toast.POSITION.TOP_CENTER,
     });
 
-    // Redirect to Dashboard after a delay
+
     setTimeout(() => {
       navigate('/dashboard');
-    }, 2000); // Delay of 2 seconds before redirecting
+    }, 2000);
   };
 
   return (
-    <div className="signup-container">
-      <ToastContainer />
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>Company Name</label>
-          <input
-            type="text"
-            name="companyName"
-            placeholder="Enter company name"
-            value={formData.companyName}
-            onChange={handleChange}
-            required
-          />
+    <div className='page-container'>
+      <div className="signup-container">
+        <ToastContainer />
+        <h2 className='pb-8'>Sign Up</h2>
+        <form onSubmit={handleSubmit} className="form-grid">
+          <div className="input-container">
+            <label>Company Name</label>
+            <input
+              type="text"
+              className="input-box"
+              name="companyName"
+              placeholder="Enter company name"
+              value={formData.companyName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Company Email</label>
+            <input
+              type="email"
+              className="input-box"
+              name="companyEmail"
+              placeholder="Enter company email"
+              value={formData.companyEmail}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Password</label>
+            <input
+              type="password"
+              className="input-box"
+              name="password"
+              placeholder="Enter password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              className="input-box"
+              name="confirmPassword"
+              placeholder="Confirm password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Address</label>
+            <input
+              type="text"
+              className="input-box"
+              name="address"
+              placeholder="Enter address"
+              value={formData.address}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Phone Number</label>
+            <input
+              type="tel"
+              className="input-box"
+              name="phoneNumber"
+              placeholder="Enter phone number"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label>Upload Documents</label>
+            <input
+              type="file"
+              name="documents"
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+        </form>
+        <div className="signup-button-container">
+          <button type="submit" className="signup-button">
+            Sign Up
+          </button>
         </div>
-        <div className="input-container">
-          <label>Company Email</label>
-          <input
-            type="email"
-            name="companyEmail"
-            placeholder="Enter company email"
-            value={formData.companyEmail}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input-container">
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input-container">
-          <label>Confirm Password</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm password"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input-container">
-          <label>Address</label>
-          <input
-            type="text"
-            name="address"
-            placeholder="Enter address"
-            value={formData.address}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input-container">
-          <label>Phone Number</label>
-          <input
-            type="tel"
-            name="phoneNumber"
-            placeholder="Enter phone number"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="input-container">
-          <label>Upload Documents</label>
-          <input
-            type="file"
-            name="documents"
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" className="signup-button">
-          Sign Up
-        </button>
-      </form>
+
+      </div>
     </div>
   );
 };
--- tally/src/components/login/login.jsx
+++ tally/src/components/login/login.jsx
@@ -14,8 +14,6 @@ const Login = () => {
     // Form validation
     if (email === 'punchbiz@kongu.edu' && password === 'admin') {
       setNotification('Login successful');
-      
-      // Navigate to the dashboard after successful login
       navigate('/dashboard');
     } else {
       setNotification('Login failed');
@@ -24,46 +22,48 @@ const Login = () => {
 
   return (
     <div className="login-wrapper">
-      <div className="login-box">
-        <h2 className="login-title">Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="input-field">
-            <label className="input-label">Email</label>
-            <input 
-              type="email" 
-              className="input-control"
-              placeholder="Enter your email" 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required 
-              pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[eE][dD][uU]$"
-              title="Please enter a valid .edu email address"
-            />
-          </div>
-          <div className="input-field">
-            <label className="input-label">Password</label>
-            <input 
-              type="password" 
-              className="input-control"
-              placeholder="Enter your password" 
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required 
-            />
-          </div>
-          <button type="submit" className="btn btn-login">
-            Login
-          </button>
-        </form>
-        <button 
-          className="btn btn-signup" 
-          onClick={() => navigate('/sign-up')}
-        >
-          Sign Up
-        </button>
-        
-        {/* Display the notification */}
-        {notification && <p className="notification">{notification}</p>}
+      <div className="outer-box">
+        <div className="login-box">
+          <h2 className="login-title">Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="input-field">
+              <label className="input-label">Email</label>
+              <input
+                type="email"
+                className="input-control"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[eE][dD][uU]$"
+                title="Please enter a valid .edu email address"
+              />
+            </div>
+            <div className="input-field">
+              <label className="input-label">Password</label>
+              <input
+                type="password"
+                className="input-control"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="login-btn ">
+              Login
+            </button>
+          </form>
+
+          {notification && <p className="notification">{notification}</p>}
+        </div>
+
+        {/* Sign Up link */}
+        <div className="signup-box">
+          <p className="signup-text">
+            Not a user? <span className="signup-link" onClick={() => navigate('/sign-up')}>Sign Up</span>
+          </p>
+        </div>
       </div>
     </div>
   );
