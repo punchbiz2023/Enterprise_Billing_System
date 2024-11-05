4296a5a139c9e3fa081235c7b3597e0825878a68
Lenovo <haris.22aim@kongu.edu>
2024-11-05 19:52:06 +0530

    sales modiefied

--- tally/src/components/Order/Order.jsx
+++ tally/src/components/Order/Order.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import SidePanel from '../Sales/Sidepanel';
+import SidePanel from '../Sales/Sidepanel.jsx';
 
 const Order = () => {
   const [salespersons, setSalespersons] = useState([]);
