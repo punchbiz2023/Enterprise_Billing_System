f6c8cff13d6ca43cb4cdaed1e4133192c320512b
Lenovo <haris.22aim@kongu.edu>
2024-11-05 19:45:52 +0530

    sales modiefied

--- tally/src/components/Expenses/expenses.jsx
+++ tally/src/components/Expenses/expenses.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import SidePanel from '../Purchase/SidePanel';
+import SidePanel from '../Purchase/Sidepanel';
 import * as XLSX from 'xlsx';
 
 const ExpenseForm = () => {
